7ceff5d389cd3b88c8ed9ceaa1599f4201567605
Larry Hawelu <lhawelu@gmail.com>
2021-02-22 16:21:18 -0800

    commit tree 08f162e0139bc794ff9271bffd2971c98004f9c0

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
-
 // Write your code here
+
+// Test number one
+const num1 = 31;
+const num2 = 2;
+const multiply = num1 * num2;
+
+// Test number two
+const random = Math.floor(Math.random() * 10) + 1;
+
+// Test number three
+const num3 = 20;
+const num4 = 8;
+const mod = num3 % num4;
+
+// Test number four
+const max = Math.max (4, 6, 3, 20, 17);
